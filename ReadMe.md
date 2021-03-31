e3aa999dbf6b765002bbff620116682153a90cdb
AshV <ashish6318@gmail.com>
2021-04-01 01:36:45 +0530

    initial commit

---
+++ ReadMe.md
@@ -0,0 +1,1 @@
+# All Emoji's List
---
+++ src/index.js
@@ -0,0 +1,97 @@
+var ASCII =
+    [
+        { "slug": "", "character": "0", "unicodeName": "0", "codePoint": "", "group": "number", "subGroup": "number" },
+        { "slug": "", "character": "1", "unicodeName": "1", "codePoint": "", "group": "number", "subGroup": "number" },
+        { "slug": "", "character": "2", "unicodeName": "2", "codePoint": "", "group": "number", "subGroup": "number" },
+        { "slug": "", "character": "3", "unicodeName": "3", "codePoint": "", "group": "number", "subGroup": "number" },
+        { "slug": "", "character": "4", "unicodeName": "4", "codePoint": "", "group": "number", "subGroup": "number" },
+        { "slug": "", "character": "5", "unicodeName": "5", "codePoint": "", "group": "number", "subGroup": "number" },
+        { "slug": "", "character": "6", "unicodeName": "6", "codePoint": "", "group": "number", "subGroup": "number" },
+        { "slug": "", "character": "7", "unicodeName": "7", "codePoint": "", "group": "number", "subGroup": "number" },
+        { "slug": "", "character": "8", "unicodeName": "8", "codePoint": "", "group": "number", "subGroup": "number" },
+        { "slug": "", "character": "9", "unicodeName": "9", "codePoint": "", "group": "number", "subGroup": "number" },
+        { "slug": "", "character": "A", "unicodeName": "A", "codePoint": "", "group": "characters", "subGroup": "capital" },
+        { "slug": "", "character": "B", "unicodeName": "B", "codePoint": "", "group": "characters", "subGroup": "capital" },
+        { "slug": "", "character": "C", "unicodeName": "C", "codePoint": "", "group": "characters", "subGroup": "capital" },
+        { "slug": "", "character": "D", "unicodeName": "D", "codePoint": "", "group": "characters", "subGroup": "capital" },
+        { "slug": "", "character": "E", "unicodeName": "E", "codePoint": "", "group": "characters", "subGroup": "capital" },
+        { "slug": "", "character": "F", "unicodeName": "F", "codePoint": "", "group": "characters", "subGroup": "capital" },
+        { "slug": "", "character": "G", "unicodeName": "G", "codePoint": "", "group": "characters", "subGroup": "capital" },
+        { "slug": "", "character": "H", "unicodeName": "H", "codePoint": "", "group": "characters", "subGroup": "capital" },
+        { "slug": "", "character": "I", "unicodeName": "I", "codePoint": "", "group": "characters", "subGroup": "capital" },
+        { "slug": "", "character": "J", "unicodeName": "J", "codePoint": "", "group": "characters", "subGroup": "capital" },
+        { "slug": "", "character": "K", "unicodeName": "K", "codePoint": "", "group": "characters", "subGroup": "capital" },
+        { "slug": "", "character": "L", "unicodeName": "L", "codePoint": "", "group": "characters", "subGroup": "capital" },
+        { "slug": "", "character": "M", "unicodeName": "M", "codePoint": "", "group": "characters", "subGroup": "capital" },
+        { "slug": "", "character": "N", "unicodeName": "N", "codePoint": "", "group": "characters", "subGroup": "capital" },
+        { "slug": "", "character": "O", "unicodeName": "O", "codePoint": "", "group": "characters", "subGroup": "capital" },
+        { "slug": "", "character": "P", "unicodeName": "P", "codePoint": "", "group": "characters", "subGroup": "capital" },
+        { "slug": "", "character": "Q", "unicodeName": "Q", "codePoint": "", "group": "characters", "subGroup": "capital" },
+        { "slug": "", "character": "R", "unicodeName": "R", "codePoint": "", "group": "characters", "subGroup": "capital" },
+        { "slug": "", "character": "S", "unicodeName": "S", "codePoint": "", "group": "characters", "subGroup": "capital" },
+        { "slug": "", "character": "T", "unicodeName": "T", "codePoint": "", "group": "characters", "subGroup": "capital" },
+        { "slug": "", "character": "U", "unicodeName": "U", "codePoint": "", "group": "characters", "subGroup": "capital" },
+        { "slug": "", "character": "V", "unicodeName": "V", "codePoint": "", "group": "characters", "subGroup": "capital" },
+        { "slug": "", "character": "W", "unicodeName": "W", "codePoint": "", "group": "characters", "subGroup": "capital" },
+        { "slug": "", "character": "X", "unicodeName": "X", "codePoint": "", "group": "characters", "subGroup": "capital" },
+        { "slug": "", "character": "Y", "unicodeName": "Y", "codePoint": "", "group": "characters", "subGroup": "capital" },
+        { "slug": "", "character": "Z", "unicodeName": "Z", "codePoint": "", "group": "characters", "subGroup": "capital" },
+        { "slug": "", "character": "a", "unicodeName": "a", "codePoint": "", "group": "characters", "subGroup": "small" },
+        { "slug": "", "character": "b", "unicodeName": "b", "codePoint": "", "group": "characters", "subGroup": "small" },
+        { "slug": "", "character": "c", "unicodeName": "c", "codePoint": "", "group": "characters", "subGroup": "small" },
+        { "slug": "", "character": "d", "unicodeName": "d", "codePoint": "", "group": "characters", "subGroup": "small" },
+        { "slug": "", "character": "e", "unicodeName": "e", "codePoint": "", "group": "characters", "subGroup": "small" },
+        { "slug": "", "character": "f", "unicodeName": "f", "codePoint": "", "group": "characters", "subGroup": "small" },
+        { "slug": "", "character": "g", "unicodeName": "g", "codePoint": "", "group": "characters", "subGroup": "small" },
+        { "slug": "", "character": "h", "unicodeName": "h", "codePoint": "", "group": "characters", "subGroup": "small" },
+        { "slug": "", "character": "i", "unicodeName": "i", "codePoint": "", "group": "characters", "subGroup": "small" },
+        { "slug": "", "character": "j", "unicodeName": "j", "codePoint": "", "group": "characters", "subGroup": "small" },
+        { "slug": "", "character": "k", "unicodeName": "k", "codePoint": "", "group": "characters", "subGroup": "small" },
+        { "slug": "", "character": "l", "unicodeName": "l", "codePoint": "", "group": "characters", "subGroup": "small" },
+        { "slug": "", "character": "m", "unicodeName": "m", "codePoint": "", "group": "characters", "subGroup": "small" },
+        { "slug": "", "character": "n", "unicodeName": "n", "codePoint": "", "group": "characters", "subGroup": "small" },
+        { "slug": "", "character": "o", "unicodeName": "o", "codePoint": "", "group": "characters", "subGroup": "small" },
+        { "slug": "", "character": "p", "unicodeName": "p", "codePoint": "", "group": "characters", "subGroup": "small" },
+        { "slug": "", "character": "q", "unicodeName": "q", "codePoint": "", "group": "characters", "subGroup": "small" },
+        { "slug": "", "character": "r", "unicodeName": "r", "codePoint": "", "group": "characters", "subGroup": "small" },
+        { "slug": "", "character": "s", "unicodeName": "s", "codePoint": "", "group": "characters", "subGroup": "small" },
+        { "slug": "", "character": "t", "unicodeName": "t", "codePoint": "", "group": "characters", "subGroup": "small" },
+        { "slug": "", "character": "u", "unicodeName": "u", "codePoint": "", "group": "characters", "subGroup": "small" },
+        { "slug": "", "character": "v", "unicodeName": "v", "codePoint": "", "group": "characters", "subGroup": "small" },
+        { "slug": "", "character": "w", "unicodeName": "w", "codePoint": "", "group": "characters", "subGroup": "small" },
+        { "slug": "", "character": "x", "unicodeName": "x", "codePoint": "", "group": "characters", "subGroup": "small" },
+        { "slug": "", "character": "y", "unicodeName": "y", "codePoint": "", "group": "characters", "subGroup": "small" },
+        { "slug": "", "character": "z", "unicodeName": "z", "codePoint": "", "group": "characters", "subGroup": "small" }
+    ];
+
+
+
+fetch("/src/list.json")
+    .then(response => response.text())
+    .then(data => {
+        loadData(JSON.parse(data));
+    })
+    .catch(error => {
+        console.log(error);
+    });
+
+function loadData(data) {
+    new gridjs.Grid({
+        columns: [{
+            id: 'character',
+            name: 'Emoji'
+        }, {
+            id: 'unicodeName',
+            name: 'Unicode Name'
+        }, {
+            id: 'group',
+            name: 'Group'
+        }, {
+            id: 'subGroup',
+            name: 'Sub Group'
+        }],
+        search: true,
+        sort: true,
+        data: data.concat(ASCII)
+    }).render(document.getElementById("wrapper"));
+}
